7866b153baf00aca44bc38ff3108d999b4319718
Snake <fireisnakei@gmail.com>
2025-03-27 13:13:55 +0300

    added output available ip addresses

--- TestEthernet/ViewModels/MainWindowViewModel.cs
+++ TestEthernet/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Runtime.CompilerServices;
 using TestEthernet.Core;
 using TestEthernet.Services;
@@ -15,14 +17,36 @@ namespace TestEthernet.ViewModels
     public class MainWindowViewModel : ViewModel
     {
 
-        #region [Переменные и их свойства]
+        #region [Переменные и их свойства]        
+
+        List<IPAddress> detectedAddresses;
+        public List<IPAddress> DetectedAddresses {
+            get => detectedAddresses;
+            set
+            {
+                detectedAddresses = value;
+                OnPropertyChanged();
+            }
+        }
 
         string outputDescription;
         public string OutputDescription
         {
-            get => outputDescription; set
+            get => outputDescription;
+            set
             {
                 outputDescription = value;
+                OnPropertyChanged();
+            }
+        }
+
+        string ipListDescription;
+        public string IpListDescription
+        {
+            get => ipListDescription;
+            set
+            {
+                ipListDescription = value;
                 OnPropertyChanged();
             }
         }
@@ -30,7 +54,8 @@ namespace TestEthernet.ViewModels
         string[] addressData = new string[2];
         public string[] AddressData
         {
-            get => addressData; set
+            get => addressData;
+            set
             {
                 addressData = value;
                 OnPropertyChanged();
@@ -40,7 +65,8 @@ namespace TestEthernet.ViewModels
         string host;
         public string Host
         {
-            get => host; set
+            get => host;
+            set
             {
                 host = value;
                 OnPropertyChanged();
@@ -50,7 +76,8 @@ namespace TestEthernet.ViewModels
         IPAddress[] address;
         public IPAddress[] Address
         {
-            get => address; set
+            get => address;
+            set
             {
                 address = value;
                 OnPropertyChanged();
@@ -86,34 +113,55 @@ namespace TestEthernet.ViewModels
         /// <summary>
         /// Метод, в котором считываются данные о текущем хосте
         /// </summary>
-        public void GetNetworkData()
+        public string GetNetworkData()
         {
+            string ipV4 = "";
             Host = Dns.GetHostName();
             Address = Dns.GetHostAddresses(Host);
 
             for (int i = 0; i < Address.Length; i++)
             {
-                if (Address[i].AddressFamily.ToString() == "InterNetworkV6")
+                if (Address[i].AddressFamily == AddressFamily.InterNetworkV6)
                 {
                     AddressData[i] = "IPv6 - ";
                 }
-                else if (Address[i].AddressFamily.ToString() == "InterNetwork")
+                else if (Address[i].AddressFamily == AddressFamily.InterNetwork)
                 {
                     AddressData[i] = "IPv4 - ";
+                    ipV4 = Address[i].ToString();
                 }
 
                 AddressData[i] = AddressData[i] + Address[i].ToString();
             }
+
+            return ipV4;
         }
 
-        public void CheckCurrentNetwork()
+
+        public List<IPAddress> CheckCurrentNetwork(string ipV4)
         {
-            foreach ((string name, OperationalStatus status) in NetworkInterface.GetAllNetworkInterfaces().Select(networkInterface =>
-            (networkInterface.Name, networkInterface.OperationalStatus)))
+            string[] parts = ipV4.Split('.');
+            ipV4 = parts[0] + "." + parts[1] + "." + parts[2] + ".";
+            int startAddress = 1;
+            int endAddress = 50;
+
+            List<IPAddress> detectedAddresses = new List<IPAddress>();
+            Ping ping = new Ping();
+
+            for(int addressPart = startAddress; addressPart<=endAddress; addressPart++)
             {
-                Debug.WriteLine(
-                    $"{name} is {status}");
+                string currentAddressAsString = $"{ipV4}{addressPart}";
+                IPAddress currentAddress = IPAddress.Parse(currentAddressAsString);
+                var pingResult = ping.Send(currentAddress, 700);
+                Debug.WriteLine($"[{DateTime.Now}] {currentAddress}: {pingResult.Status}");
+
+                if (pingResult.Status == IPStatus.Success)
+                {
+                    detectedAddresses.Add(currentAddress);
+                }
             }
+
+            return detectedAddresses;
         }
 
 
@@ -125,18 +173,25 @@ namespace TestEthernet.ViewModels
 
             try
             {
-                GetNetworkData();
+                string ipV4 = GetNetworkData();
                 OutputDescription = "Адреса хоста: ";
+                DetectedAddresses = CheckCurrentNetwork(ipV4);
+                if (detectedAddresses.Count != 0)
+                {
+                    IpListDescription = "Доступные IP адреса";
+                }
+                else
+                {
+                    IpListDescription = "Нет доступных IP адресов";
+                }
             }
             catch
             {
                 OutputDescription = "Ошибка подключения сети";
             }
-            CheckCurrentNetwork();
+            
         }
 
         public RelayCommand NavigateCurrentNetwork { get; set; }
-
-
     }
 }
