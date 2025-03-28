1c64edd8844c4c3772153c718fb7d2d15af6f256
Snake <fireisnakei@gmail.com>
2025-03-28 11:27:45 +0300

    added method that calc mac address from ip

--- TestEthernet/ViewModels/MainWindowViewModel.cs
+++ TestEthernet/ViewModels/MainWindowViewModel.cs
@@ -19,6 +19,17 @@ namespace TestEthernet.ViewModels
 
         #region [Переменные и их свойства]
 
+        string[] detectedMacs;
+        public string[] DetectedMacs
+        {
+            get => detectedMacs;
+            set
+            {
+                detectedMacs = value;
+                OnPropertyChanged();
+            }
+        }
+
         string[] detectedHosts;
         public string[] DetectedHosts
         {
@@ -118,6 +129,45 @@ namespace TestEthernet.ViewModels
         protected void OnPropertyChanged([CallerMemberName] string name = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public string GetMacAddress(string ipAddress)
+        {
+            string macAddress = string.Empty;
+            Process pProcess = new Process();
+            pProcess.StartInfo.FileName = "arp";
+            pProcess.StartInfo.Arguments = "-a " + ipAddress;
+            pProcess.StartInfo.UseShellExecute = false;
+            pProcess.StartInfo.RedirectStandardOutput = true;
+            pProcess.StartInfo.CreateNoWindow = true;
+            pProcess.Start();
+            string strOutput = pProcess.StandardOutput.ReadToEnd();
+            string[] substrings = strOutput.Split('-');
+            if (substrings.Length >= 8)
+            {
+                macAddress = substrings[3].Substring(Math.Max(0, substrings[3].Length - 2))
+                         + "-" + substrings[4] + "-" + substrings[5] + "-" + substrings[6]
+                         + "-" + substrings[7] + "-"
+                         + substrings[8].Substring(0, 2);
+                return macAddress;
+            }
+
+            else
+            {
+                return "Неопределено";
+            }
+        }
+
+        public string[] GetMacsArray(List<IPAddress> detectedAddresses)
+        {
+            string[] s = new string[detectedAddresses.Count];
+
+            for (int i = 0; i < detectedAddresses.Count; i++)
+            {
+                s[i] = GetMacAddress(detectedAddresses[i].ToString());
+            }
+
+            return s;
         }
 
         /// <summary>
@@ -238,6 +288,7 @@ namespace TestEthernet.ViewModels
                 {
                     IpListDescription = "Доступные IP адреса:";
                     DetectedHosts = GetNamesArray(detectedAddresses);
+                    DetectedMacs = GetMacsArray(detectedAddresses);
                 }
                 else
                 {
