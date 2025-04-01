e2319fbf8752910460a5438a9c7db991fac95758
AngelikaSuhareva <asuhareva356@gmail.com>
2025-04-02 00:11:11 +0300

    added server service

---
+++ ServerService/Program.cs
@@ -0,0 +1,14 @@
+﻿using ServerService.Services;
+using System.Diagnostics;
+namespace ServerService
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            var server = new NetworkServerService();
+            await server.StartAsync(8080);
+            Debug.WriteLine("Сервер запущен. Нажмите любую клавишу для остановки...");
+        }
+    }
+}
---
+++ ServerService/Services/ServerService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+using ServerService.Models;
+using System.Diagnostics;
+
+namespace ServerService.Services
+{
+    public class NetworkServerService
+    {
+        private TcpListener _listener;
+        private CancellationTokenSource _cts;
+
+        public async Task StartAsync(int port)
+        {
+            _cts = new CancellationTokenSource();
+            _listener = new TcpListener(IPAddress.Any, port);
+            _listener.Start();
+
+            while (!_cts.IsCancellationRequested)
+            {
+                var client = await _listener.AcceptTcpClientAsync();
+                _ = ProcessClientAsync(client);
+            }
+        }
+
+        private async Task ProcessClientAsync(TcpClient client)
+        {
+            using (var stream = client.GetStream())
+            {
+                var buffer = new byte[1024];
+                int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+                var data = buffer.Take(bytesRead - 2).ToArray();
+                var receivedCrc = BitConverter.ToUInt16(buffer, bytesRead - 2);
+
+                if (Crc16.Calculate(data) == receivedCrc)
+                {
+                    Debug.WriteLine("Данные не повреждены.");
+                }
+            }
+        }
+    }
+}
